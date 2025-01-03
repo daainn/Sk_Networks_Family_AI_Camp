d134c8321c603c1a089ce00fc3f070904848c666
Dain Lee <109786068+daainn@users.noreply.github.com>
2025-01-03 10:24:18 +0900

    crawling데이터를 mysql과 연동하는 실습

--- Crawling_MYSQL_Connector_Practice/naver_book_sql.py
+++ Crawling_MYSQL_Connector_Practice/naver_book_sql.py
@@ -14,7 +14,7 @@ sql = "INSERT INTO naver_book (book_title, book_image, author,  publisher, isbn,
 
 for book_info in naver_book.book_list:
     values = (book_info['title'], book_info['image'],  book_info['author'],
-              book_info['publisher'], book_info['isbn'], book_info['description'], book_info['pubdate'])
+              book_info['publisher'], str(book_info['isbn']), book_info['description'], book_info['pubdate'])
     cursor.execute(sql, values)
 
 connection.commit()
